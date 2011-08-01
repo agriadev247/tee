3520ee187f4c8d60a5f4aebd2d5554e156efe8f7
agriadev247 <agriadev247@gmail.com>
2011-08-01 12:14:17 +0200

    added region.tpl.php

---
+++ templates/region.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * The tee theme implementation to display a region.
+ */
+?>
+<?php if ($content): ?>
+<!-- <?php print $region; ?> region begin -->
+<?php print $content; ?>
+<!-- <?php print $region; ?> region end -->
+<?php endif; ?>
